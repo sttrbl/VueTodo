69ae030fc9be7369991667329ffff5733933b7a1
Stanislav <stasmark21@gmail.com>
2019-07-24 16:45:07 +0300

    Minor changes to logic and code style

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
-Vue.component('task-row', {
+const taskRow = {
 	props: ['task'],
 	template: `<li 
-						   class="task-list__item task"
-						   :class="{task_completed: task.state === 'completed'}"
-						 >
-							 <button 
-							   class="task__controller"
-							   @click = "$emit('change-task-state', task.id)"
-							 ></button>
+				class="task-list__item task"
+				:class="{task_completed: task.state === 'completed'}"
+				>
+					<button 
+					class="task__controller"
+					@click = "$emit('change-task-state', task.id)"
+					></button>
 
-							 <span class="task__text">{{task.text}}</span>
-							 
-							 <button 
-							   class="task__remove-button"
-							   @click = "$emit('remove-task', task.id)"
-							 ></button>
-						 </li>`
-});
+					<span class="task__text">{{task.text}}</span>
+					
+					<button 
+					class="task__remove-button"
+					@click = "$emit('remove-task', task.id)"
+					></button>
+				</li>`
+};
 
 
 let vm = new Vue({
-	el:'#app',
+	el: '.app',
+	components: {
+		taskRow
+	},
 	data: {
 		lastId: null,
 		allTasks: null,
 		tabs: [
-						['all', 'Все'], 
-						['active', 'Активные'], 
-						['completed', 'Завершенные']
-					],
+			['all', 'Все'],
+			['active', 'Активные'],
+			['completed', 'Завершенные']
+		],
 		currentTab: 'all',
 		newTask: ''
 	},
 
 	methods: {
-		updateStorage: function() {
+		updateStorage: function () {
 			localStorage.setItem('tasks', JSON.stringify(this.allTasks));
 			localStorage.setItem('lastId', this.lastId);
 		},
 
-		getTaskObjIndex: function(objId) {
-			return this.allTasks.findIndex( task => {
-			  return task.id == objId;	
+		getTaskObjIndex: function (objId) {
+			return this.allTasks.findIndex(task => {
+				return task.id == objId;
 			});
 		},
 
-		validateNewTaskField: function() {
+		validateNewTaskField: function () {
 			return !!this.newTask.trim();
 		},
 
-		addTask: function() {
-			if ( !this.validateNewTaskField() ) return;
+		addTask: function () {
+			if (!this.validateNewTaskField()) return;
 
 			this.allTasks.push({
 				id: ++this.lastId,
@@ -61,28 +64,31 @@ let vm = new Vue({
 			this.newTask = '';
 		},
 
-		removeTask: function(taskId) {
+		removeTask: function (taskId) {
 			const taskObjIndex = this.getTaskObjIndex(taskId);
 
 			this.allTasks.splice(taskObjIndex, 1);
 		},
 
-		changeTaskState: function(taskId) {
+		changeTaskState: function (taskId) {
 			const taskObjIndex = this.getTaskObjIndex(taskId);
 			const taskObj = this.allTasks[taskObjIndex];
 
-			taskObj.state = taskObj.state == 'active' ? 'completed' : 'active'; 
+			taskObj.state = taskObj.state == 'active' ? 'completed' : 'active';
 		},
 
-		removeCurrentTaskCategory: function() {
+		removeCurrentTaskCategory: function () {
 			const category = this.currentTab;
 
-			if (category == 'all') return this.allTasks = [];
+			if (category == 'all') {
+				this.allTasks = [];
+				return;
+			}
 
 			this.allTasks = category == 'active' ? this.completedTasks : this.activeTasks;
 		},
 
-		checkAllTasks: function() {
+		checkAllTasks: function () {
 			this.activeTasks.forEach(item => {
 				item.state = 'completed';
 			});
@@ -91,41 +97,39 @@ let vm = new Vue({
 
 
 	watch: {
-	  allTasks: {
-	  	handler: function() {
-	  		this.updateStorage();
-	  	},
-	  	deep: true
+		allTasks: {
+			handler: function () {
+				this.updateStorage();
+			},
+			deep: true
 		}
 	},
 
 
 	computed: {
-		currentTaskObjName: function() {
+		currentTaskObjName: function () {
 			return this.currentTab + 'Tasks';
 		},
 
-		activeTasks: function() {
-			return this.allTasks.filter( (task, i) => {
+		activeTasks: function () {
+			return this.allTasks.filter((task, i) => {
 				return task.state == 'active' ? true : false;
 			});
 		},
 
-		completedTasks: function() {
-			return this.allTasks.filter( (task, i) => {
+		completedTasks: function () {
+			return this.allTasks.filter((task, i) => {
 				return task.state == 'completed' ? true : false;
 			});
 		},
-
 	},
 
-	created: function() {
-		 const storageTasks = localStorage.getItem('tasks');
-		 const storageLastId = localStorage.getItem('lastId');
+	created: function () {
+		const storageTasks = localStorage.getItem('tasks');
+		const storageLastId = localStorage.getItem('lastId');
 
-		 this.allTasks = storageTasks ? JSON.parse(storageTasks) : [];
-		 this.lastId = storageLastId ? storageLastId : 0;   
+		this.allTasks = storageTasks ? JSON.parse(storageTasks) : [];
+		this.lastId = storageLastId ? storageLastId : 0;
 	}
 
 });
-
